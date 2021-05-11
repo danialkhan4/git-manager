1d7acbc6b35059839a1220b6e1048a6d3aef86e4
danialk <48042585+danialkhan4@users.noreply.github.com>
2021-05-11 01:58:43 -0700

    updated frontend - added bootstrap

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import config
 from routes.auth import auth
 from flask import Flask, render_template, redirect, url_for, session, request, json
 from flask_oauthlib.client import OAuth
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -20,6 +21,18 @@ github = oauth.remote_app(
     access_token_url='https://github.com/login/oauth/access_token',
     authorize_url='https://github.com/login/oauth/authorize'
 )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
@@ -51,11 +64,11 @@ def authorized():
     session['github_token'] = (resp['access_token'], '')
     me = github.get('user')
     print(me.data)
-    return redirect(url_for('manage'))
+    return redirect(url_for('dashboard'))
 
 
-@app.route('/manage')
-def manage():
+@app.route('/dashboard')
+def dashboard():
     # check if user is authenticated
     if 'github_token' in session:
         user = github.get('user')
@@ -67,7 +80,7 @@ def manage():
         except requests.ConnectionError:
             return "Connection Error"
 
-        return render_template('manage.html', user=user, repos=response)
+        return render_template('dashboard.html', user=user, repos=response)
 
     return redirect(url_for('login'))
 
